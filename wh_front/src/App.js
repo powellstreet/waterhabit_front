4715bece78956c292b48fc33c6ef94deb80fbe6a
powellstreet <codestates.jc@gmail.com>
2020-07-13 23:35:28 +0900

    :zap: SpeedDial 위치 App 컴포넌트로 변경
    - 버튼별 세부 링크 연결 필요

--- wh_front/src/App.js
+++ wh_front/src/App.js
@@ -11,7 +11,52 @@ import Profile from "./components/Profile";
 import MainBar from "./components/MainBar";
 import Footer from "./components/Footer";
 
+import { makeStyles } from "@material-ui/core";
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@material-ui/lab";
+import { FileCopyOutlined, Save, Print, Share, Favorite } from "@material-ui/icons";
+
+
+const useStyles = makeStyles((theme) => ({
+  speedDial: {
+    position: "absolute",
+    "&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft": {
+      bottom: theme.spacing(20),
+      right: theme.spacing(15),
+    },
+    "&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight": {
+      top: theme.spacing(20),
+      left: theme.spacing(15),
+    },
+    exampleWrapper: {
+      position: "relative",
+      marginTop: theme.spacing(3),
+      height: 380,
+    },
+  },
+}));
+
+const actions = [
+  { icon: <FileCopyOutlined />, name: "Copy" },
+  { icon: <Save />, name: "Save" },
+  { icon: <Print />, name: "Print" },
+  { icon: <Share />, name: "Share" },
+  { icon: <Favorite />, name: "Like" },
+];
+
 function App() {
+  const classes = useStyles();
+
+  const [hidden, setHidden] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -25,6 +70,27 @@ function App() {
           <Route path="/mainBar" component={MainBar} />
         </Switch>
         <Footer />
+        <div className={classes.exampleWrapper}>
+          <SpeedDial
+            ariaLabel="SpeedDial example"
+            className={classes.speedDial}
+            hidden={hidden}
+            icon={<SpeedDialIcon />}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            open={open}
+            direction="up"
+          >
+            {actions.map((action) => (
+              <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+                onClick={handleClose}
+              />
+            ))}
+          </SpeedDial>
+        </div>
       </div>
     </BrowserRouter>
   );
